f5a6a56d49b21af6ecb9791fde1dd5f20548bdef
nixiaohui <13941835+nixiaohui@users.noreply.github.com>
2023-10-30 19:48:43 +0800

    美丽彩虹

---
+++ rainbow.py
@@ -0,0 +1,45 @@
+import turtle as t
+
+t.speed(10)
+t.pensize(25)
+t.lt(90)
+t.color('red')
+t.circle(160, 180)
+
+t.penup()
+t.goto(-20, 0)
+t.pendown()
+t.lt(180)
+t.color('orange')
+t.circle(140, 180)
+
+t.penup()
+t.goto(-40, 0)
+t.pendown()
+t.lt(180)
+t.color('yellow')
+t.circle(120, 180)
+
+t.penup()
+t.goto(-60, 0)
+t.pendown()
+t.lt(180)
+t.color('green')
+t.circle(100, 180)
+
+t.penup()
+t.goto(-80, 0)
+t.pendown()
+t.lt(180)
+t.color('blue')
+t.circle(80, 180)
+
+t.penup()
+t.goto(-100, 0)
+t.pendown()
+t.lt(180)
+t.color('purple')
+t.circle(60, 180)
+
+
+t.done()
